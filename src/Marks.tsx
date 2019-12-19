4a996f97b2d2cd388c3a35850e574704253cac23
Joshua Pollak <jpollak@zynga.com>
2019-12-19 10:34:01 -0800

    proper marks layout

---
+++ src/Marks.tsx
@@ -0,0 +1,8 @@
+import OpenIconicNames from "./OpenIconicNames";
+
+export const MARKS: Array<[string, OpenIconicNames]> = [
+    ["heart", OpenIconicNames.heart],
+    ["pin", OpenIconicNames.pin],
+    ["questionable", OpenIconicNames["question-mark"]],
+    ["bug", OpenIconicNames.bug],
+];
