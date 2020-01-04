042293b119782e470640f4a84dacb5bec7c69a74
Joshua Pollak <josh@Mac-e0accb991ed6.pico.org>
2020-01-03 23:44:31 -0800

    formatting

--- src/IWordRecord.tsx
+++ src/IWordRecord.tsx
@@ -12,34 +12,35 @@ export interface ITags {
 }
 
 export type ResultTags<T> =
-{ [K in keyof PlainProperties<T>]?: ITags } &
-{ [K in keyof MapProperties<T>]?: {[key: string]:
-     T[K] extends {[key: string]: any[]} ? ITags[] : ITags }} &
-{ [K in keyof ArrayProperties<T>]?: ITags[] };
+    { [K in keyof PlainProperties<T>]?: ITags } &
+    { [K in keyof MapProperties<T>]?: {
+        [key: string]: T[K] extends { [key: string]: any[]; } ? ITags[] : ITags;
+    } } &
+    { [K in keyof ArrayProperties<T>]?: ITags[] };
 
 type DiscardedDictionaryEntry = MultipleValue<IDictionaryEntry, "example" | "etymology">;
 
 export default interface IWordRecord {
-  q: string;
-  re: RetrieveEntry;
-  result?: Partial<IDictionaryEntry>;
-  resultTags?: ResultTags<IDictionaryEntry>;
-  resultOriginal?: Partial<IDictionaryEntry>;
-  resultDiscarded?: Partial<DiscardedDictionaryEntry>;
-  resultDiscardedTags?: ResultTags<DiscardedDictionaryEntry>;
-  allTags?: ITags;
-  pipelineNotes?: string[];
-  notes: string;
+    q: string;
+    re: RetrieveEntry;
+    result?: Partial<IDictionaryEntry>;
+    resultTags?: ResultTags<IDictionaryEntry>;
+    resultOriginal?: Partial<IDictionaryEntry>;
+    resultDiscarded?: Partial<DiscardedDictionaryEntry>;
+    resultDiscardedTags?: ResultTags<DiscardedDictionaryEntry>;
+    allTags?: ITags;
+    pipelineNotes?: string[];
+    notes: string;
 }
 
 export interface IDiscardedWordRecord {
-  q: IWordRecord["q"];
-  re: IWordRecord["re"];
-  result?: IWordRecord["resultDiscarded"];
-  resultTags?: IWordRecord["resultDiscardedTags"];
-  resultDiscarded?: IWordRecord["resultDiscarded"];
-  resultDiscardedTags?: IWordRecord["resultDiscardedTags"];
-  allTags?: IWordRecord["allTags"];
-  pipelineNotes?: IWordRecord["pipelineNotes"];
-  notes: IWordRecord["notes"];
+    q: IWordRecord["q"];
+    re: IWordRecord["re"];
+    result?: IWordRecord["resultDiscarded"];
+    resultTags?: IWordRecord["resultDiscardedTags"];
+    resultDiscarded?: IWordRecord["resultDiscarded"];
+    resultDiscardedTags?: IWordRecord["resultDiscardedTags"];
+    allTags?: IWordRecord["allTags"];
+    pipelineNotes?: IWordRecord["pipelineNotes"];
+    notes: IWordRecord["notes"];
 }
