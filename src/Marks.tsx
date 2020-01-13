fda0425aa6d7ff6fa17834e08c05010680fe4316
Joshua Pollak <josh@Mac-e0accb991ed6.pico.org>
2020-01-13 15:02:22 -0800

    add "bad" mark

--- src/Marks.tsx
+++ src/Marks.tsx
@@ -3,6 +3,7 @@ import OpenIconicNames from "./OpenIconicNames";
 const Marks: Array<[string, OpenIconicNames]> = [
     ["heart", OpenIconicNames.heart],
     ["checked", OpenIconicNames.check],
+    ["bad", OpenIconicNames["thumb-down"]],
     ["pin", OpenIconicNames.pin],
     ["questionable", OpenIconicNames["question-mark"]],
     ["objectionable", OpenIconicNames.warning],
