03404a90c1b721319af25e5cffc806439ea9961c
Joshua Pollak <josh@Freda.corp.zynga.com>
2020-02-20 11:17:51 -0800

    cleanup

--- src/__tests__/redis.test.ts
+++ src/__tests__/redis.test.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import Redis from "ioredis";
-// const { promisify } = require("util");
 
 const BS_PREAMBLE_LUA: string = fs.readFileSync("./src/redis/BS.preamble.lua", { encoding: "UTF-8" });
 const BSADD_LUA: string = BS_PREAMBLE_LUA + fs.readFileSync("./src/redis/BSADD.lua", { encoding: "UTF-8" });
@@ -14,9 +13,6 @@ function expectError(promise: Promise<any>, matcher: string | RegExp) {
 describe("BSADD", () => {
     const client = new Redis({ db: 5 });
     beforeAll(async (cb) => await client.flushdb().then(cb.bind(null, undefined)));
-
-    // const getAsync = promisify(client.get).bind(client);
-    // const setAsync = promisify(client.set).bind(client);
 
     test("source exists", () => {
         expect(BSADD_LUA).toBeDefined();
@@ -86,8 +82,8 @@ describe("BSADD", () => {
     });
 
     test("BSREM", async (cb) => {
-        let item = "goodbye";
-        let mark = "heart";
+        const item = "goodbye";
+        const mark = "heart";
         expect((await client.smembers(markKey(mark))).sort()).toEqual(["goodbye", "hello"]);
         expect(await client.eval(BSREM_LUA, 2, itemKey(item), markKey(mark), item, mark)).toEqual([1, 1]);
         expect((await client.smembers(markKey(mark))).sort()).toEqual(["hello"]);
